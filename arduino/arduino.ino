ba08e05eaf4ce3a93ce7258db5782563eff0152a
cwjpalmer <cwjpalmer@gmail.com>
2017-01-29 18:05:18 +0100

    Arduino sending data to mac over serial, python receiving data and writing to CSV

---
+++ arduino/arduino.ino
@@ -0,0 +1,20 @@
+// tell the Arduino you're using the Awesome Shield library
+#include <Awesome.h>
+// tell the Arduino you're using the Awesome Shield hardware
+Awesome awesome;
+
+// declare variables here
+
+
+void setup() {
+  awesome.setup(9600);
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  Serial.print("Light:");
+  Serial.println(awesome.lightSensor.reading());
+  delay(1000);
+}
---
+++ readSerial.py
@@ -0,0 +1,33 @@
+#!/usr/local/bin/python
+
+import serial
+import time
+import datetime
+
+data = []
+
+i = 0
+
+ser = serial.Serial('/dev/cu.usbmodem621',9600)
+
+log = open("log.csv","a+")
+
+while True:
+	received_line=ser.readline().rstrip()
+	timestamp = datetime.datetime.fromtimestamp(time.time()).strftime('%Y-%m-%d %H:%M:%S')
+	entry = [timestamp]
+	entry.append(received_line.split(':')[0])
+	entry.append(received_line.split(':')[1])
+	data.append(entry)
+	# print myList
+	print data[i]
+
+	with open("log.csv","a+") as myfile:
+		number_of_columns_in_entry = len(data[i])
+		for x in range (0,number_of_columns_in_entry):
+    			myfile.write(str(data[i][x]))
+    			if x < number_of_columns_in_entry-1:
+    					myfile.write(",")
+	with open("log.csv","a+") as myfile:
+    		myfile.write("\n")
+	i += 1
